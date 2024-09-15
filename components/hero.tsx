4cb4abae1103b2084ae6a3b62b84e79b60266d83
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-09-15 22:26:42 +0300

    landing & hero pages

---
+++ components/hero.tsx
@@ -0,0 +1,58 @@
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
+export const Hero = () => {
+  return (
+    <div className="pt-20">
+      <div className="text-center px-8">
+        <div className="mb-6">
+          <div className="relative inline-flex before:absolute before:inset-0">
+            <Link
+              href="https://www.google.com"
+              className="px-3 py-1 text-sm font-medium inline-flex items-center 
+              justify-center border rounded-full text-zinc300 hover:text-white transition 
+              duration-15- ease-in-out w-full group border-slate-100/40"
+              target="_blank"
+            >
+              <span className="relative inline-flex items-center">
+                socials{" "}
+                <span
+                  className="tracking-normal text-primary-500 group-hover:translate-x-0.5 
+                  transition-transform duration-150 ease-in-out ml-1"
+                >
+                  -&gt;
+                </span>
+              </span>
+            </Link>
+          </div>
+        </div>
+        <h1
+          className="pd-4 font-extrabold tracking-tight text-transparent text-7xl
+          lg:text-8xl bg-clip-text bg-gradient-to-r from-cyan-100 via-blue-700 to-cyan-100"
+        >
+          Hi am mohammed
+        </h1>
+        <p className="mb-8 text-lg text-zinc-300/40 font-medium">
+          I am a full-stack developer, I build web applications using React,
+          Next.js, Node.js, Drupal, Mysql and MongoDB.
+        </p>
+        <div className="flex flex-col items-center max-w-xs mx-auto gap-4 sm:justify-center sm:flex-row sm:inline-flex">
+          <Link
+            href="#calendy"
+            className="w-full items-center justify-center flex whitespace-nowrap transition 
+            duration-150 ease-in-out font-medium rounded px-4 py-1.5 text-zinc-900 bg-gradient-to-r 
+            from-white/80 via-white to-white/80 hover:bg-white group"
+          >
+            Book a free call{" "}
+            <ArrowRight
+              className="w-3 h-3 tracking-normal text-primary-500 group-hover:translate-x-0.5 
+              transition-transform duration-150 ease-in-out ml-1"
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ components/landing.tsx
@@ -0,0 +1,13 @@
+"use client"; // This file will be executed on the client only
+
+export const Landing = () => {
+  return (
+    <div>
+      <div className="pt-32 pb-16 md:pt-52 mb:pd-32 relative max-w-5xl min-h-screen px-4 mx-auto sm:px-6 space-y-80">
+        hello
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
