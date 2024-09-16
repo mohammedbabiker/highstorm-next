95c2085065d2c24fad5f33f65a92cc574659e65d
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-09-16 15:10:45 +0300

    fade animation

--- components/landing.tsx
+++ components/landing.tsx
@@ -1,10 +1,23 @@
-"use client"; // This file will be executed on the client only
+"use client";
+import { useEffect } from "react";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Hero from "../components/hero";
 
 export const Landing = () => {
+  useEffect(() => {
+    AOS.init({
+      disable: "mobile", // phone
+      duration: 800,
+      easing: "ease-out-cubic",
+    });
+  });
+
   return (
     <div>
       <div className="pt-32 pb-16 md:pt-52 mb:pd-32 relative max-w-5xl min-h-screen px-4 mx-auto sm:px-6 space-y-80">
-        hello
+        <Hero />
       </div>
     </div>
   );
