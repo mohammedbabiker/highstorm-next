dea9ba127cf983c6e29075bbc371f5eb50a9acf5
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-09-24 18:06:20 +0300

    add footer section

---
+++ components/footer.tsx
@@ -0,0 +1,41 @@
+import EmailIcon from "@mui/icons-material/Email";
+import XIcon from "@mui/icons-material/X";
+import Link from "next/link";
+// import YouTubeIcon from "@mui/icons-material/YouTube";
+// import InstagramIcon from "@mui/icons-material/Instagram";
+// import TwitterIcon from "@mui/icons-material/Twitter";
+export const Footer = () => {
+  const links = [
+    {
+      title: "Home",
+      href: "https://www.youtube.com/channel/UCv1fFr7v9sE7bZjZb7H4i8w",
+      icon: <XIcon className="h-8 w-8" />,
+    },
+    {
+      title: "About",
+      href: "/about",
+      icon: "",
+    },
+    {
+      href: "malito:babai20@protonmail.ch",
+      icon: <EmailIcon className="w-8 h-8" />,
+    },
+  ];
+  return (
+    <div className="text-center pb-8">
+      <div className="flex text-center justify-center space-x-4">
+        {links.map((link, index) => (
+          <Link
+            key={index}
+            href={link.href}
+            className="hover:opacity-45 transition duration-300"
+          >
+            {link.icon}
+          </Link>
+        ))}
+      </div>
+      <p className="text-sm text-slate-100/50 pt-3">All right reserved</p>
+    </div>
+  );
+};
+export default Footer;
--- components/landing.tsx
+++ components/landing.tsx
@@ -6,6 +6,7 @@ import "aos/dist/aos.css";
 import Calendy from "../components/calendy";
 import Channels from "../components/channels";
 import EditVideo from "../components/edited-video";
+import Footer from "../components/footer";
 import Hero from "../components/hero";
 
 export const Landing = () => {
@@ -24,6 +25,9 @@ export const Landing = () => {
         <EditVideo />
         <Channels />
         <Calendy />
+        <div className="footer-section">
+          <Footer />
+        </div>
       </div>
     </div>
   );
